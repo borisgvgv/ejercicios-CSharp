92f377a97606e56a56c369807353181db4e03654
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2023-10-09 19:13:33 +0000

    act

---
+++ 40-1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Ejercicio_40_1
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduce un número:");
+        int numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Introduce otro número:");
+        int numero2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("¿A partir de qué número buscamos?");
+        int limiteInferior = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("¿Hasta qué número buscamos?");
+        int limiteSuperior = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"He encontrado los siguientes múltiplos comunes de ambos números:");
+
+        bool encontrados = false; // Variable para verificar si se encontraron múltiplos comunes
+
+        for (int i = limiteInferior; i <= limiteSuperior; i++)
+        {
+            if (i % numero1 == 0 && i % numero2 == 0)
+            {
+                Console.Write($"{i} ");
+                encontrados = true;
+            }
+        }
+
+        if (!encontrados)
+        {
+            Console.WriteLine("Ninguno");
+        }
+
+        Console.WriteLine(); // Salto de línea al final
+    }
+}
--- 40/Program.cs
+++ 40/Program.cs
@@ -1,24 +1,27 @@
 ﻿/*
-39. Escribe un programa que le pida al usuario dos números a y b, 
-así como un límite inferior y un límite superior, y muestre todos 
-los números entre ambos límites (incluidos) que sean a la vez múltiplos 
-de a y de b, como en este ejemplo:
+40. Crea una versión mejorada del ejercicio anterior: 
+Escribe un programa que le pida al usuario dos números a y b, 
+así como un límite inferior y un límite superior, 
+y muestre todos los números entre ambos límites 
+(incluidos) que sean a la vez múltiplos de a y de b, 
+o la palabra "Ninguno" si corresponde, como en este ejemplo:
 
 Introduce un número: 2
-
 Introduce otro número: 5
-
 ¿A partir de qué número buscamos? 10
-
 ¿Hasta qué número buscamos? 30
-
 He encontrado los siguientes múltiplos comunes de ambos números: 10 20 30
 
+Introduce un número: 2
+Introduce otro número: 5
+¿A partir de qué número buscamos? 11
+¿Hasta qué número buscamos? 19
+He encontrado los siguientes múltiplos comunes de ambos números: Ninguno
 *///Boris Garcés Vernier
 
 using System;
 
-class Ejercicio_39
+class Ejercicio_40
 {
     static void Main()
     {
@@ -35,7 +38,7 @@ class Ejercicio_39
         int limiteSuperior = Convert.ToInt32(Console.ReadLine());
 
         int num1, num2, limInf, limSup;
-
+        bool encontrados = false;
 
         if ((numero1 > numero2) || (limiteInferior > limiteSuperior))
         {
@@ -53,25 +56,31 @@ class Ejercicio_39
             limSup = limiteSuperior;
         }
 
-        Console.Write($"{num1} {num2} ");
-        Console.WriteLine($"{limInf} {limSup} ");
+        Console.WriteLine($"número introducidos: {num1} y {num2}");
+        Console.WriteLine($"intervalo definido entre: {limInf} y {limSup} ");
 
+        Console.Write("Números que intervienen: ");
+        for (int i = limInf; i <= limSup; i++)
+        {
+            System.Console.Write($"{i} ");
+        }
+        System.Console.WriteLine();// Salto de línea
+
+Console.Write("Múltiplos obtenidos: ");
         for (int i = limInf; i <= limSup; i++)
         {
 
             if ((i % num1 == 0) && (i % num2 == 0))
             {
                 System.Console.Write($"{i} ");
-
+                encontrados = true;
             }
-
+        }
+        if(!encontrados){
+        System.Console.WriteLine("Ninguno");
         }
 
         Console.WriteLine();//Salto de línea.
-
-
-
-
 
     }
 }
