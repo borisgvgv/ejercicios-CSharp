c4c9982672eec31a18f8d3ae48c6c921a6f021b0
Boris Garcés <borisgarces@imac-de-boris.home>
2023-10-16 12:27:25 +0200

    act

---
+++ 41/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+41. Pide al usuario dos números enteros y muestra su división. 
+Usa excepciones para comprobar los posibles errores.
+*/ //Boris Garcés Vernier
+using System;
+class E41
+{
+    static void Main()
+    {
+        try
+        {
+            System.Console.WriteLine("Introduce un número");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+            System.Console.WriteLine("Introduce un número");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = numero1 / numero2;
+            System.Console.WriteLine("Su división es: {0}", resultado);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("No es un número válido");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("No se puede dividir entre cero");
+        }
+    }
+}
---
+++ 42/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+42. Muestra un triángulo creciente, formado por "almohadillas" (#), 
+alineado a la derecha, con el tamaño indicado por el usuario, usando "for":
+
+Tamaño? 5
+    #
+   ##
+  ###
+ ####
+#####
+*/ //Boris Garcés Vernier
+
+using System;
+class E42
+{
+    static void Main()
+    {
+
+        System.Console.WriteLine("Introduce un número");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        int numeroIn = numero;
+
+        for (int i = 1; i <= numero; i++)
+        {
+
+            for (int j = 1; j <= numeroIn - 1; j++)
+            {
+                System.Console.Write(" ");
+            }
+            for (int ii = numeroIn; ii < numero + 1; ii++)
+            {
+                System.Console.Write("#");
+            }
+
+            numeroIn--;
+            System.Console.WriteLine();
+        }
+
+    }
+}
