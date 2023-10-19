3427a3eb6309bea20ce5b672f97caead6e9b4895
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2023-10-19 08:06:41 +0000

    act

---
+++ 49/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+49. Haz un programa que dé al usuario la oportunidad
+de adivinar un número del 1 al 100 (que estará prefijado en el programa)
+en un máximo de 6 intentos. En cada pasada deberá avisarle de si 
+se ha pasado o se ha quedado corto.
+*/ //Boris Garcés Vernier
+
+using System;
+class E49
+{
+    static void Main()
+    {
+
+        int numOculto = 34;
+        int intentos = 1;
+
+        Console.WriteLine("Introduce un número");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+
+        while ((numero != numOculto)|| (intentos < 6))
+        {
+
+            if (numero < numOculto)
+            {
+                Console.WriteLine("El número es muy bajo");
+            }
+            else if (numero > numOculto)
+            {
+                Console.WriteLine("El número es muy alto");
+            }
+            else if (intentos == 6)
+            {
+                Console.WriteLine("Has superado el número de intentos posibles");
+            }
+            else
+            {
+                Console.WriteLine("El número número es correcto.");
+            }
+            Console.WriteLine("Introduce un número");
+            numero = Convert.ToInt32(Console.ReadLine());
+            intentos++;
+        }
+
+
+
+    }
+}
